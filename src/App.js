eeef2be1d1774c6dcda3dcd40080d8fc0b452fb8
boleynen <58037455+boleynen@users.noreply.github.com>
2021-03-30 12:46:52 +0200

    covered step 2/12: useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-import Setup from './tutorial/1-useState/setup/3-useState-array'
+import Setup from './tutorial/2-useEffect/setup/3-useEffect-fetch-data'
+// import Final from './tutorial/2-useEffect/final/1-useEffect-basics'
 
 function App() {
   return (
     <div className='container'>
       <Setup/>
+      {/* <Final/> */}
     </div>
   )
 }
--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,7 +3,26 @@ import React, { useState, useEffect } from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+
+  // werkt elke keer als er op de btn wordt geklikt, omdat de meegegeven array nie leeg is
+  useEffect(()=>{
+    if(value>=1){
+      document.title = `New Messages (${value})`
+    }
+  }, [value]);
+
+  // werkt maar 1 keer on page load, array is leeg
+  useEffect(()=>{
+    console.log('hello world')
+  }, [])
+
+  return(
+    <>
+      <h1>{value}</h1>
+      <button className="btn" onClick={()=> setValue(value+1)}>Click me!</button>
+    </>
+    )
 };
 
 export default UseEffectBasics;
