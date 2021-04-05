d49cbcdf79be836f54a62a6df466492d61eae73d
boleynen <58037455+boleynen@users.noreply.github.com>
2021-04-05 13:37:00 +0200

    covered step 6/12: useReducer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Setup from './tutorial/5-useRef/setup/1-useRef-basics'
+import Setup from './tutorial/6-useReducer/setup'
 // import Final from './tutorial/3-conditional-rendering/final/1-multiple-returns'
 
 function App() {
